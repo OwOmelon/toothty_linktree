952fc4556d10e865e489f01fef289bd6e939a66f
melon <cccaaarrrlll99@gmail.com>
2025-04-30 22:38:31 +0800

    initial commit

---
+++ src/components/Links.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import IconYoutube from "~icons/ri/youtube-fill";
+import IconInstagram from "~icons/ri/instagram-fill";
+import IconX from "~icons/ri/twitter-x-fill";
+import IconTiktok from "~icons/ri/tiktok-fill";
+import IconTwitch from "~icons/ri/twitch-fill";
+
+const links = [
+	{
+		icon: IconYoutube,
+	},
+	{
+		icon: IconInstagram,
+	},
+	{
+		icon: IconX,
+	},
+	{
+		icon: IconTiktok,
+	},
+	{
+		icon: IconTwitch,
+	},
+];
+</script>
+
+<template>
+	<div class="flex grow text-night-dark">
+		<div
+			v-for="link in links"
+			class="outline-night-light bg-night-light hover:text-night-light grid w-full place-items-center py-[0.625em] outline-[0.125em] -outline-offset-[0.125em] outline-solid hover:bg-transparent"
+		>
+			<component :is="link.icon" class="h-[4em] w-[4em]" />
+		</div>
+	</div>
+</template>
---
+++ src/components/Pfp.vue
@@ -0,0 +1,7 @@
+<template>
+	<img
+		src="@/assets/pfp.webp"
+		alt="toothty profile picture"
+		class="border-night-light outline-night-light aspect-square h-full border-[0.125em] object-cover outline-[0.125em] -outline-offset-[0.875em] outline-solid"
+	/>
+</template>
