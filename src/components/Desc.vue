99309800efded5d99131792952885d5819ab058f
melon <cccaaarrrlll99@gmail.com>
2025-05-01 14:36:24 +0800

    create description component

---
+++ src/components/Desc.vue
@@ -0,0 +1,11 @@
+<template>
+	<div
+		class="desc lg:bg-night-light lg:text-night-dark col-[1/-1] grid grid-cols-[min-content,_min-content,_auto] flex-col gap-[0.75em] font-semibold lg:col-start-3 lg:row-start-1 lg:flex lg:justify-center lg:px-3"
+	>
+		<p>age • pronouns</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur. Vulputate platea sit non erat
+			curabitur
+		</p>
+	</div>
+</template>
