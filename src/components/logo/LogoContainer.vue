aaa0038df2f0ec86fe58d1ef20030469f6aabf7c
melon <cccaaarrrlll99@gmail.com>
2025-05-01 14:36:03 +0800

    move logo related components to "logo" folder

---
+++ src/components/logo/LogoContainer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Logo_Horizontal from "./Logo_Horizontal.vue";
+import Logo_Vertical from "./Logo_Vertical.vue";
+</script>
+
+<template>
+	<div class="logo-container bg-night-light grid place-items-center lg:p-[1em]">
+		<Logo_Vertical class="lg:hidden" />
+		<Logo_Horizontal class="hidden lg:block" />
+	</div>
+</template>
