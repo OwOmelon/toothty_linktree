7e78d06d0e5536e9605e3e4172e89e32dabe1781
melon <cccaaarrrlll99@gmail.com>
2025-05-07 18:59:31 +0800

    <GrainOverlay>: increase opacity and base frequency

--- src/components/GrainOverlay.vue
+++ src/components/GrainOverlay.vue
@@ -1,12 +1,12 @@
 <template>
 	<svg class="hidden">
 		<filter id="grain">
-			<feTurbulence type="turbulence" baseFrequency="0.33" />
+			<feTurbulence type="turbulence" baseFrequency="0.66" />
 		</filter>
 	</svg>
 
 	<div
 		style="filter: url(#grain) grayscale(100%)"
-		class="pointer-events-none fixed top-0 left-0 z-50 h-full w-full opacity-25"
+		class="pointer-events-none fixed top-0 left-0 z-50 h-full w-full opacity-40"
 	/>
 </template>
