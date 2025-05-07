a1cc4ff423e02cedb4c455f6f1c8662d900087f8
melon <cccaaarrrlll99@gmail.com>
2025-05-07 09:50:54 +0800

    valo bg

---
+++ src/components/ValoBg.vue
@@ -0,0 +1,17 @@
+<!-- ACCOUNT FOR SMALLER SCREEN SIZES -->
+
+<template>
+	<div
+		class="fixed top-0 left-0 -z-[999] flex h-full w-full flex-col opacity-25 mix-blend-soft-light"
+	>
+		<img src="@/assets/valo_breeze.png" alt="valorant breeze map" />
+	</div>
+</template>
+
+<style scoped>
+@reference "@/assets/main.css";
+
+.valo-bg {
+	background-image: url("@/assets/valo_breeze.png");
+}
+</style>
