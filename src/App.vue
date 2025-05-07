e6de25f10e4914d2ed2d13ae740c3fbac387ccfe
melon <cccaaarrrlll99@gmail.com>
2025-05-07 18:59:55 +0800

    designs

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,59 @@ import YoruJapaneseText from "@/designs/bg/YoruJapaneseText.vue";
 import YoruDesc from "@/designs/bg/YoruDesc.vue";
 import ColumnOfEllipses from "@/designs/bg/ColumnOfEllipses.vue";
 
+import TopLeftSquares from "@/designs/content/TopLeftSquares.vue";
+import BunchaLines from "@/designs/content/BunchaLines.vue";
+import TopRightCornerOutline from "@/designs/content/TopRightCornerOutline.vue";
+import BottomRightDottedQuarterEllipse from "@/designs/content/BottomRightDottedQuarterEllipse.vue";
+
 import ValoBg from "@/components/ValoBg.vue";
 import GrainOverlay from "@/components/GrainOverlay.vue";
+
+// =============================
+
+/*import { onMounted } from "vue";
+
+let app: HTMLElement | null = null;
+
+onMounted(() => {
+	app = document.getElementById("app")
+
+	window.onmousemove = (e) => {
+		if (!app) return;
+
+		const { clientX, clientY } = e;
+		const windowWidthHalf = window.innerWidth / 2;
+		const windowHeightHalf = window.innerHeight / 2;
+
+		const fromCenterX = (clientX - windowWidthHalf) / windowWidthHalf;
+		const fromCenterY = (clientY - windowHeightHalf) / windowHeightHalf;
+
+		const maxTranslate = 1
+		const translateX = `${fromCenterX * maxTranslate}rem`
+		const translateY = `${fromCenterY  * maxTranslate}rem`
+
+		app.style.setProperty("--dot-grid-translate-x", translateX)
+		app.style.setProperty("--dot-grid-translate-y", translateY)
+	};
+});*/
 </script>
 
 <template>
 	<ValoBg />
 
 	<div
-		class="drop-shadow-night-light/25 lg:rows-[auto_auto] relative isolate grid w-[34.75em] grid-cols-[61%_auto] gap-[1.25em] text-[min(2.5vw,_1rem)] drop-shadow-[0_0_0.25em] lg:w-[54.375em] lg:grid-cols-[33.33%_min-content_auto] lg:gap-0 lg:text-base"
+		class="drop-shadow-night-light/25 lg:rows-[auto_auto] relative isolate grid w-[34.75em] grid-cols-[61%_auto] gap-[1.25em] text-[min(2vw,_1rem)] drop-shadow-[0_0_0.25em] lg:w-[54.375em] lg:grid-cols-[33.33%_min-content_auto] lg:gap-0 lg:text-base"
 	>
 		<Pfp />
 		<LogoContainer />
 		<Links />
 		<Desc />
-	</div>
 
+		<TopLeftSquares />
+		<BunchaLines />
+		<TopRightCornerOutline />
+		<BottomRightDottedQuarterEllipse />
+	</div>
 
 	<YoruJapaneseText />
 	<YoruDesc />
---
+++ src/designs/content/TopLeftSquares.vue
@@ -0,0 +1,14 @@
+<template>
+	<svg
+		width="1.5625em"
+		height="3.8125em"
+		viewBox="0 0 25 61"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		class="absolute right-full bottom-full -translate-y-[1.25em]"
+	>
+		<path d="M0 0H25V25H0V0Z" fill="#D9D9D9" />
+		<path d="M17 35H25V43H17V35Z" fill="#D9D9D9" />
+		<path d="M17 53H25V61H17V53Z" fill="#D9D9D9" />
+	</svg>
+</template>
